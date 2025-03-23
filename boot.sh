9eabe15ceb117497fade4e602be076ccca0a6b75
cartesv <cartesv@gmail.com>
2025-03-23 19:38:13 -0300

    .

--- boot.sh
+++ boot.sh
@@ -54,6 +54,7 @@ bash $BOOT_SCRIPTS_PATH/install/install_gc_logging.sh
 
 ############################################################################################################
 # Se actualiza la configuración de zona horaria de Py (sin horario de invierno)
+# dummy
 bash $BOOT_SCRIPTS_PATH/install/install_py_tz_change.sh
 ############################################################################################################
 
