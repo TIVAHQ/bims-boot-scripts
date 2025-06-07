1764ba83dd3fa98cd5f7f6216a022a983bd3ef74
cartesv <cartesv@gmail.com>
2025-06-07 12:14:46 -0300

    Remount bucket

--- boot.sh
+++ boot.sh
@@ -106,3 +106,7 @@ if ! pgrep -x "httpd" > /dev/null; then
 else
     echo "Apache está corriendo correctamente."
 fi
+
+umount /mnt/bims-bucket-1
+fusermount -u /mnt/bims-bucket-1
+gcsfuse --implicit-dirs --file-mode 777 --dir-mode 777 -o allow_other bims-bucket-1 /mnt/bims-bucket-1
